c822e756d347ed36b098bddcb64acd8d38c8a203
Gintautas <svedgintas@gmail.com>
2020-02-13 16:40:38 +0200

    Added various extensions.

---
+++ UriExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace JobAds.Extensions
+{
+    // Uri extensions for simplier url manipulation.
+    public static class UriExtensions
+    {
+        // Set Url parameter for the url.
+        public static Uri SetQueryValue(this Uri uri, string name, object value)
+        {
+            NameValueCollection nvc = HttpUtility.ParseQueryString(uri.Query);
+            nvc[name] = (value ?? string.Empty).ToString();
+
+            return new UriBuilder(uri) { Query = nvc.ToString() }.Uri;
+        }
+
+        // Increase numerical query parameter by one.
+        public static Uri IncreaseQueryIntegerValue(this Uri uri, string name)
+        {
+            var websiteParam = HttpUtility.ParseQueryString(uri.Query)
+                .Get(name);
+
+            int.TryParse(websiteParam, out int paramValue);
+
+            paramValue++;
+
+            return uri.SetQueryValue(name, paramValue);
+        }
+
+
+
+    }
+}
